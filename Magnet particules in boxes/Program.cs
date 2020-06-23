b9431626f5faebb6f3d0bac6a86740e63c12fb98
Yaroslav <Yaroslav.Rybak64@gmail.com>
2020-06-23 15:36:01 +0300

    Magnet particules in boxes (4 Kata)

---
+++ Magnet particules in boxes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magnet_particules_in_boxes
+{
+    public class Magnets
+    {
+        public static double Doubles(int maxk, int maxn)
+        {
+            double output = 0;
+            for (int i = 1; i <= maxk; i++)
+                for (int j = 1; j <= maxn; j++)
+                    output += 1 / (i * Math.Pow((j + 1), i * 2));
+            return output;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magnets.Doubles(1, 10);
+        }
+    }
+}
