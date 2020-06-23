cb4070f2f8025c6298be7de883ac54cd5a1b4987
Yaroslav <Yaroslav.Rybak64@gmail.com>
2020-06-23 15:14:56 +0300

    add Product of consecutive Fib numbers (5 Kata)

---
+++ Product of consecutive Fib numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_of_consecutive_Fib_numbers
+{
+    public class ProdFib
+    {
+        public static ulong[] productFib(ulong prod)
+        {
+            ulong F0 = 0;
+            ulong F1 = 1;
+            ulong temp;
+            ulong product = F0*F1;
+            while(product < prod)
+            {
+                temp = F0;
+                F0 = F1;
+                F1 = F0 + temp;
+
+                product = F0 * F1;
+            }
+            if (product == prod)
+                return new ulong[] { F0, F1, 1 };
+            else 
+                return new ulong[] { F0, F1, 0 };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProdFib.productFib(4895);
+        }
+    }
+}
