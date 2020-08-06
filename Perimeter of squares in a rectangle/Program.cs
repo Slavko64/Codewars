84d44c368c6951143d386a3a1975401e2e671bc9
unknown <Yaroslav.Rybak64@gmail.com>
2020-08-06 22:10:50 +0300

    add Perimeter of squares in a rectangle (Kata 5)

---
+++ Perimeter of squares in a rectangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Perimeter_of_squares_in_a_rectangle
+{
+    public class SumFct
+    {
+        public static BigInteger perimeter(BigInteger n)
+        {
+            BigInteger a = 1, b = 1, per = 8*a;
+            for (int i = 0; i < n-1; i++)
+            {
+                a += b;
+                b = a - b;
+                per += 4*a;
+            }
+            return per;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumFct.perimeter(7));
+            Console.ReadLine();
+        }
+    }
+}
