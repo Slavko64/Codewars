76a1d799594d6eaa956b046d252867bbf1e3266c
Yaroslav Rybak <Yaroslav.Rybak64@gmail.com>
2021-02-10 12:24:29 +0200

    add Weight for weight 5 Kata

---
+++ Weight for weight/Weight for weight/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weight_for_weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123");
+            Console.Read();
+        }
+    }
+}
+public class WeightSort
+{
+
+    public static string orderWeight(string strng) => strng.Split(' ').ToArray().OrderBy(x => x).OrderBy(x => x.ToArray().Sum(y => int.Parse(y.ToString()))).Aggregate((x, y) => x + " " + y).ToString();
+
+}
